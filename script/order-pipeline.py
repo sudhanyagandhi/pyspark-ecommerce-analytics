24516da357ad417e65449c4d438062a3074467ca
sudhanyagandhi <mail2sudhanyagandhi@gmail.com>
2025-04-30 23:40:40 +0530

    Update order-pipeline.py

--- script/order-pipeline.py
+++ script/order-pipeline.py
@@ -2,26 +2,20 @@ from pyspark.sql import SparkSession
 from pyspark.sql.functions import col,sum,round
 #from pyspark.sql.types import FloatType
 
-
-
 spark = SparkSession.builder.appName("App").getOrCreate()
 
 #load Data
 df = spark.read.csv("file:///home/cdpadmin/data/orders.csv",header=True,inferSchema=True)
 
-
 #clean Data
-
 df_clean = df.dropna(subset=['order_id','product_id','customer_id','amount'],how="any")\
              .withColumn('amount',col('amount').cast("float"))
 #df_clean.show()
 #df_clean.printSchema()
 
 #total revenue per product
-
 df_revenue = df_clean.groupBy("product_id")\
-                     .agg(round(sum('amount'),2).alias("revenue"))\
-                     #.withColumn("revenue",round("revenue",2))
+                     .agg(round(sum('amount'),2).alias("revenue"))
                      
 df_revenue.show()
 #df_revenue.printSchema()
